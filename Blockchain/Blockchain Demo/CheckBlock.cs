354b0a9ccc78499018d2e4c131605843502e7c85
janis070487 <janis070487@gmail.com>
2022-06-09 23:25:55 +0300

    Pārbaude tika uzlabota

---
+++ Blockchain/Blockchain Demo/CheckBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blockchain_Demo
+{
+    public static class CheckBlock
+    {
+        public static bool Check(int how, string stringHash)
+        {
+            for (int a = 0; a < how; a++) // Pārbauda vai ir vajadzīgās nules
+            {
+                if (stringHash[a] != '0') // ja kārtējais pārbaudāmais skaitlis neatbilst vajadzīgajam tad atgriež false
+                {
+                    return false;
+                }
+            }
+                return true; // bet ja iziet parbaudi tad atgriež true
+        }
+    }
+}
--- Blockchain/Blockchain Demo/Core.cs
+++ Blockchain/Blockchain Demo/Core.cs
@@ -31,39 +31,18 @@ namespace Blockchain_Demo
             }
             Check(4);   // pārbauda vai ir vajadzīgais rezultāts
         }
-        public void Check(int how) // pārbauda vai bloki ir pareizi
+        public void Check(int how)
         {
-            int counter = 0;
-            for(int i = 0; i < blockchain.block.Length; i++) //Pārbauda katru bloku vai atbilst vajadzīgajam paveiktajam darbam
+            for (int i = 0; i < blockchain.block.Length; i++) //Pārbauda katru bloku vai atbilst vajadzīgajam paveiktajam darbam
             {
-                for(int a = 0; a < how; a++) // Pārbauda vai ir vajadzīgās nules katram blokam
-                {
-                    if (blockchain.block[i].blockTxt.hashSum[a] == '0') // ja kārtējais pārbaudāmais skaitlis atbilst vajadzigajam
-                         // tad rezultatam pieskaita viens
-                    {
-                        counter++;
-                    }
-                    else
-                    {
-                        blockchain.block[i].status = false; // Ja neatbilst tad statusu ieliek folse
-                        break;                              // un pārtrauc siklu
-                    }
-                }
-                if(how == counter)   // ja netika partraukts cikls tad pārbauda vai ir vajadzīgo simbolu skaits ja jā
-                {
-                    blockchain.block[i].status = true; // tad parbaudāmā cikla statusu ieststa true
-                }
-                counter = 0; // skaititāju iestata nules vērtībā lai nakamo bloku parbauditu
-
+                blockchain.block[i].status = CheckBlock.Check(how, blockchain.block[i].blockTxt.hashSum);
             }
-        }
+         }
         public void LoadFirstDataInBloc()
         {
            for(int i = 0; i < blockchain.block.Length; i++)
             {
                 blockchain.block[i].blockTxt.nonce = FirstData.Data[i];
-               // blockchain.block[i].blockTxt.prewHashSum = FirstData.Data[i + 1];
-                //blockchain.block[i].blockTxt.hashSum = FirstData.Data[i + 2];
             }
         }
     
